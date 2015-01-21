5cdf43efaa0a99c2ddcff79ed5538c4cc4047536
solitarily <chuangweida@gmail.com>
2015-01-21 21:29:22 +0800

    update for mkl

--- mokee.mk
+++ mokee.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/e975/e975.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := e975
-PRODUCT_NAME := cm_e975
+PRODUCT_NAME := mk_e975
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-E975
 PRODUCT_MANUFACTURER := LGE
